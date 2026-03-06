e541bef7e79cccec837c2a31c97fc5d1e8f5a1a2
Muwonge Richard <rmuwonge054@gmail.com>
2026-03-06 19:47:24 +0300

    Fixed C# exercises to match specifications

--- Exercise2-Conditionals/Program.cs
+++ Exercise2-Conditionals/Program.cs
@@ -4,16 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter a number: ");
-        int number = int.Parse(Console.ReadLine());
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
 
-        if (number % 2 == 0)
+        string letter = "";
+
+        if (grade >= 90)
         {
-            Console.WriteLine("The number is even.");
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
         }
         else
         {
-            Console.WriteLine("The number is odd.");
+            letter = "F";
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter);
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time.");
         }
     }
 }
